7c76255b84318570690f79bad515752da17672f3
rodneywells01 <rodneywells01@gmail.com>
2016-06-28 22:54:25 -0400

    db update

--- includes/connection.php
+++ includes/connection.php
@@ -5,11 +5,11 @@
  //  define("DB_NAME", "nickwebsite");
   
 
-define("DB_SERVER", "aa1h2kiicn6toyg.cvpgnxpp51py.us-west-2.rds.amazonaws.com");
-define("DB_USER", "rodneywells01");
-define("DB_PASS", "justham9");
-define("DB_NAME", "ebdb");
-define("DB_PORT", "3306");
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "PIPQ56wDWQ");
+define("DB_NAME", "nickdb");
+// define("DB_PORT", "3306");
 
 // define("DB_SERVER", "aa18jnukjbpxjzm.cvpgnxpp51py.us-west-2.rds.amazonaws.com");
 // define("DB_USER", "rodneywells01");
@@ -20,7 +20,7 @@ define("DB_PORT", "3306");
   // 1. Create a database connection
   //$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 try {
-  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
