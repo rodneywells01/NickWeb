f81bb582824c010757671bb6622b9ee08d7e589b
rodneywells01 <rodneywells01@gmail.com>
2016-06-30 18:13:08 -0400

    testing new email service

---
+++ emailtest.php
@@ -0,0 +1,7 @@
+<?php 
+	if (mail("rodneywells01@gmail.com", "Holy Shit PHP Is Bae", "Can't believe that that actually worked. ")) {
+		echo "Ohhhhhh snapppppp";
+	} else {
+		echo "FAILURE FAILLURE";
+	}
+?>
